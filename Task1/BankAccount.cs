818a48fdd554378f99bf1b6cc1c944c887558eca
Vlad <gresko.97@mail.ru>
2024-01-12 20:43:06 +0300

    Add project files.

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class BankAccount<T>
+    {
+        private T accountNumber;
+        private decimal balance;
+        private string ownerFullName;
+
+
+        public T AccountNumber
+        {
+            get { return accountNumber; }
+            private set { accountNumber = value; }
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set
+            {
+                if (value <0) 
+                {
+                    balance = 0;
+                }
+                else
+                {
+                    balance=value;
+                }
+            }
+        }
+
+        public string OwnerFullName
+        {
+            get { return ownerFullName; }
+            set { ownerFullName = value; }
+        }
+
+        public BankAccount(T accountNumber, decimal balance, string ownerFullName)
+        {
+
+            AccountNumber = accountNumber;
+            Balance = balance;
+            OwnerFullName = ownerFullName;
+        }
+
+        public decimal AddToBalance(decimal additive)
+        {
+            Balance = balance + additive;
+            return Balance;
+        }
+
+        public decimal RemoveFromBalance(decimal subtrahend) 
+        {
+            Balance = balance - subtrahend;
+            return Balance;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>(1,100,"Иванов Иван Иванович");
+            BankAccount<string> bankAccount2 = new BankAccount<string>("02", 100, "Петров Петр Петрович");
+            bankAccount1.AddToBalance(200);
+            bankAccount2.AddToBalance(300);
+            Console.WriteLine($"На счету под номером {bankAccount1.AccountNumber} лежит {bankAccount1.Balance}");
+            Console.WriteLine($"На счету под номером {bankAccount2.AccountNumber} лежит {bankAccount2.Balance}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
